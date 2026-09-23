53fc338366b89fde888cb298e96f78d7a55425cd
Piyush Purohit <piyushpurohitoffi10@gmail.com>
2026-09-23 11:20:13 +0530

    Time: 0 ms (100%), Space: 12.4 MB (55.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -5,22 +5,21 @@ public:
         int j = n - 1;
         int k = m + n - 1;
         while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;
+                k--;
+            }
+            else {
                 nums1[k] = nums1[i];
                 i--;
                 k--;
             }
-            else {
+            }
+            while (j >= 0) {
                 nums1[k] = nums2[j];
                 j--;
                 k--;
-            }
-
+                }
         }
-        while (j >= 0) {
-                nums1[k] = nums2[j];
-                j--;
-                k--;
-            }
-    }
 };
